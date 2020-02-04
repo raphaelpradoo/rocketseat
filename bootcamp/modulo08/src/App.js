89cf832673abb2bdedc35223688ca7a23b4d27d0
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-02-04 14:03:01 -0300

    Finalização dos exercícios do Módulo 08

---
+++ bootcamp/modulo08/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+
+function App() {
+  const [tech, setTech] = useState([]);
+  
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if(storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
+  return (
+    <>
+      <ul>
+        { tech.map(t => <li key={t}>{t}</li>) }
+      </ul>
+      <strong>Voce tem {techSize} tecnologias</strong><br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Adicionar</button>
+    </>
+  );
+}
+
+export default App;
