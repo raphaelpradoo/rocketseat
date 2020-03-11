a2cd79a2e566fff982a52612b4c2076fb5109c37
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-03-11 15:21:42 -0300

    Módulo 10 - Exercicios

---
+++ bootcamp/modulo10/src/index.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import './config/ReactotronConfig';
+
+import { store, persistor } from './store';
+import App from './App';
+
+export default function Index() {
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <App />
+        </PersistGate>
+      </Provider>
+    </NavigationContainer>
+  );
+}
