5a3b9134a469b9039ee2d688e0fc9f702b2c4028
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-03-30 15:07:45 -0300

    Desafio Final - Primeira Carga de Dados

---
+++ bootcamp/fastfeet/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // Aplicacao apta a receber requisicoes no formato JSON
+    this.server.use(express.json());
+  }
+
+  routes() {
+    // Importanto as rotas do arquivo routes.js
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
