8d7b430e31a80b0bf44c4d2af83e31b9a40aab61
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-04-04 13:10:39 -0300

    Remoção de projetos inuteis

--- bootcamp/fastfeet/backend/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: process.env.APP_SECRET,
-  expiresIn: '30d',
-};
--- bootcamp/meetapp/src/config/auth.js
+++
@@ -1,6 +0,0 @@
-export default {
-  secret: 'ab3f690bf341d0c8e11b2f0b7051969a',
-  expiresIn: '7d',
-};
-
-//meetapprocketseat
