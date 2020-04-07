9df227d036008dbbcfcff38c65116b196f81e5b2
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-04-07 08:11:44 -0300

    Persist de Autenticação

--- bootcamp/desafiofinal/fastfeet/frontend/src/store/modules/auth/sagas.js
+++ bootcamp/desafiofinal/fastfeet/frontend/src/store/modules/auth/sagas.js
@@ -1,23 +1,29 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signInFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  yield put(signInSuccess(token, user));
+    yield put(signInSuccess(token, user));
 
-  history.push('/dashboard');
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Falha na autenticação. Verifique seus dados.');
+    yield put(signInFailure());
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
